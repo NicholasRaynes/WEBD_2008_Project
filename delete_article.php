d1e94458c6a90f8e889d216fea0a3cef8d31c09e
Nicholas Raynes <nicholasraynesmb@gmail.com>
2022-11-09 17:52:32 -0600

    Add delete_article.php

---
+++ delete_article.php
@@ -0,0 +1,21 @@
+<?php
+    require('authenticate.php');
+    require('connect.php');
+
+    if(isset($_GET['id']))
+    {
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        $query = "DELETE FROM articles WHERE id = :id";
+
+        $statement = $db->prepare($query);
+
+        $statement->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $statement->execute();
+        
+        $row = $statement->fetch(); 
+
+        header("Location: index.php");
+    }
+?>
