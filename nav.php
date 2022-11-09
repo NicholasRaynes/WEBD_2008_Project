db6680509dd6297d3dba71f7d0a57c9401920ba9
Nicholas Raynes <nicholasraynesmb@gmail.com>
2022-11-09 17:57:15 -0600

    Add nav.php

---
+++ nav.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script src="https://kit.fontawesome.com/d4de3d3a72.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php"><i class="fa-solid fa-basketball" style="color:orange"></i> HoopsPlus+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php"><i class="fa-solid fa-house"></i> Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="nba_stats.php"><i class="fa-solid fa-database"></i> NBA Stats</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="fan_forum.php"><i class="fa-solid fa-list"></i> Fan Forum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="creator_hub.php"><i class="fa-solid fa-door-open"></i> Creator Hub</a>
+      </li>
+    </ul>
+    </div>
+  </div>
+</nav>
