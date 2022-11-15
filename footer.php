239ecc937b87770ee8d48c5736b32bc2b78a9da3
Nicholas Raynes <nicholasraynesmb@gmail.com>
2022-11-14 18:31:38 -0600

    Removed fixed position of footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-light text-center text-lg-start fixed-bottom foot">
+<footer class="bg-light text-center text-lg-start foot">
   <div class="text-center p-3 bg-light">
     © 2022 Copyright:
     <a class="text-dark" href="index.php">HoopsPlus.com</a>
