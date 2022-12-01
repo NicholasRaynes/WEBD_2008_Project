3c2d13aa05e539e92f666aa218318107f2bea706
Nicholas Raynes <nicholasraynesmb@gmail.com>
2022-11-30 21:19:40 -0600

    Added access_concern.php

---
+++ access_concern.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Success!</title>
+</head>
+<body>
+	<div class="text-center">
+		<h1 id="success_message">Only specific users can perform this function, or access this page. Please login or sign up.</h1>
+		<button id="home" type="button" class="btn btn-primary" onclick="location.href='index.php'">Home</button>
+	</div>
+</body>
+</html>
