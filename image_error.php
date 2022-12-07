7290c28cc7ce4e7083cd35bfd30e8250582553a8
Nicholas Raynes <nicholasraynesmb@gmail.com>
2022-12-06 20:16:55 -0600

    Added image_error.php

---
+++ image_error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Image Upload Error!</title>
+</head>
+<body>
+	<div class="text-center">
+		<h1 id="success_message">There was an issue with your selected image file. Please ensure that your image is in a JPG, PNG, or JPEG format.</h1>
+		<button id="home" type="button" class="btn btn-primary" onclick="location.href='index.php'">Home</button>
+	</div>
+</body>
+</html>
